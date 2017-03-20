9a205e4ea828cd1657c0a0c627763e8bb2367180
yothinix <m@yothinix.com>
2017-03-20 14:16:09 +0700

    Test actions :rocket:

--- src/actions/__tests__/todos.test.js
+++ src/actions/__tests__/todos.test.js
@@ -3,5 +3,13 @@ import { addTodo } from '../todos'
 
 describe('Todos actions', () => {
   it('creates ADD_TODO action when calling addTodo()', () => {
+    const actual = addTodo('Sleep')
+    const expected = {
+      type: ADD_TODO,
+      payload: {
+        text: 'Sleep',
+      },
+    }
+    expect(actual).toEqual(expected)
   })
 })
---
+++ src/actions/todos.js
@@ -0,0 +1,6 @@
+import { ADD_TODO } from './types'
+const addTodo = text => ({
+  type: ADD_TODO,
+  payload: { text },
+})
+export { addTodo }
