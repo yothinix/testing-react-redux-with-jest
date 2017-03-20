068db69edb525bb4b085e72d91121e74aa1cbea8
yothinix <m@yothinix.com>
2017-03-20 10:10:22 +0700

    TodoItem component :rocket:

---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React, { PropTypes } from 'react'
+import style from './TodoItem.css'
+
+const TodoItem = ({ text, complete }) => (
+  <li className={complete ? style.complete : ''}>{text}</li>
+)
+
+TodoItem.propTypes = {
+  text: PropTypes.string.isRequired,
+  complete: PropTypes.bool,
+}
+
+TodoItem.defaultProps = {
+  complete: false,
+}
+
+export default TodoItem
+
