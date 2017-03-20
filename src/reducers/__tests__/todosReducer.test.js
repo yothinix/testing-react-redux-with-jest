3c3005136aee88d6e3278432875e967bbfd4468f
yothinix <m@yothinix.com>
2017-03-20 14:31:41 +0700

    Fix Test todoReducer :rocket:

--- src/reducers/__tests__/todosReducer.test.js
+++ src/reducers/__tests__/todosReducer.test.js
@@ -1,3 +1,4 @@
+import { ADD_TODO } from '../../actions/types'
 import todosReducer from '../todosReducer'
 
 describe('todosReducer', () => {
@@ -12,5 +13,21 @@ describe('todosReducer', () => {
     const nextState = todosReducer(curState, {})
     expect(nextState).toBe(curState)
     expect(nextState).toEqual(curState)
+  })
+
+  it('adds todos when receiving ADD_TODO action', () => {
+    const curState = [
+      { id: 1, text: 'Feed cat', complete: false },
+    ]
+    const action = {
+      type: ADD_TODO,
+      payload: { text: 'Sleep' },
+    }
+    const nextState = todosReducer(curState, action)
+    const expectedState = [
+      { id: 1, text: 'Feed cat', complete: false },
+      { id: 2, text: 'Sleep', complete: false },
+    ]
+    expect(nextState).toEqual(expectedState)
   })
 })
--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,9 +1,22 @@
+import { ADD_TODO } from '../actions/types'
+
 const initialState = []
 
 const todosReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_TODO: {
+      const nextId = 1 + state.reduce(
+        (max, cur) => Math.max(max, cur.id), 0)
+      return [...state, {
+        id: nextId,
+        text: action.payload.text,
+        complete: false,
+      }]
+    }
     default:
       return state
+  }
 }
 
 export default todosReducer
+
