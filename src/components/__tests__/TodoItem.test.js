4d2544e38ecb7ad3c2e94d882e96863909e46405
yothinix <m@yothinix.com>
2017-03-20 11:58:54 +0700

    Add enzyme-to-json for testing snapshot :rocket:

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import TodoItem from '../TodoItem'
 
 describe('TodoItem', () => {
+  it('matches its snapshot - not complete', () => {
+    const component = shallow(<TodoItem text="Not complete" />)
+
+    expect(toJson(component)).toMatchSnapshot()
+  })
+
   it('renders correct structure', () => {
     const component = shallow(<TodoItem text="Hello" />)
 
     expect(component.is('li')).toBe(true)
+    expect(component.text()).toBe('Hello')
   })
 
   it('has complete class if receiving complete prop', () => {
